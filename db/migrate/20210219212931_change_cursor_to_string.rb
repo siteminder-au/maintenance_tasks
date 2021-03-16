c7b8198a00e5096c28b11d28982137bfb582fe76
Sam Bostock <sambostock@users.noreply.github.com>
2021-03-16 10:29:27 -0400

    Update db/migrate/20210219212931_change_cursor_to_string.rb
    Co-authored-by: Adrianna Chang <adrianna.chang@shopify.com>

--- db/migrate/20210219212931_change_cursor_to_string.rb
+++ db/migrate/20210219212931_change_cursor_to_string.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 class ChangeCursorToString < ActiveRecord::Migration[6.1]
-  def change
-    reversible do |dir|
-      change_table(:maintenance_tasks_runs) do |t|
-        dir.up   { t.change(:cursor, :string) }
-        dir.down { t.change(:cursor, :bigint) }
-      end
+  def up
+    change_table(:maintenance_tasks_runs) do |t|
+      t.change(:cursor, :string)
+    end
+  end
+
+
+    def down
+    change_table(:maintenance_tasks_runs) do |t|
+      t.change(:cursor, :bigint)
     end
   end
 end
