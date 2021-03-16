600cb5fe03107e23cca0c19a4217b99a7e10084e
Sam Bostock <sam.bostock@shopify.com>
2021-03-16 10:32:20 -0400

    Tweak migration
    - Use minimum version of AR (6.0)
    - Add comment
    - Fix whitespace

--- db/migrate/20210219212931_change_cursor_to_string.rb
+++ db/migrate/20210219212931_change_cursor_to_string.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
-class ChangeCursorToString < ActiveRecord::Migration[6.1]
+
+class ChangeCursorToString < ActiveRecord::Migration[6.0]
+  # This migration will clear all existing data in the cursor column for
+  # both MySQL and PostgresQL. Ensure no Tasks are paused when this migration
+  # is deployed, or they will be resumed from the start.
+  # Running tasks are able to gracefully handle this change, even if
+  # interrupted.
   def up
     change_table(:maintenance_tasks_runs) do |t|
       t.change(:cursor, :string)
     end
   end
 
-
-    def down
+  def down
     change_table(:maintenance_tasks_runs) do |t|
       t.change(:cursor, :bigint)
     end
